aa499d82c3340d8fd0b6c0e6f8ad37ea254459fb
Tomeu Ramis <tomeuramis99@yahoo.es>
2020-11-16 21:20:23 +0100

    cositas

--- button.py
+++ button.py
@@ -1,19 +1,37 @@
 from graphics import Rectangle,Point,Text,color_rgb
+
 class Button:
-    def __init__(self,x,y,w,h,text,toggle,func):
-        self.text = Text(Point(x,y), text)
+
+    def __init__(self,x,y,w,h,text,toggle):       
         self.rect = Rectangle(Point(x,y), Point(x+w, y+h))
+        self.text = Text(self.rect.getCenter(), text)
         self.toggle = toggle
         self.pressed = False
-        self.func = func
+        self.func = None
         
-    def press(self):
+    def press(self,win):
         if(self.toggle):
             self.pressed = not self.pressed
         self.func(self.pressed)
+        self.draw(win)
 
     def draw(self,win):
         if(self.pressed):
-            self.rect.setFill(color_rgb(170,170,170))
-        else:
             self.rect.setFill(color_rgb(100,100,100))
+        else:
+            self.rect.setFill(color_rgb(170,170,170))
+        self.rect.undraw()
+        self.rect.draw(win)
+        self.text.undraw()
+        self.text.draw(win)
+
+    def isInside(self,x,y):
+        xmin = self.rect.getP1().getX()
+        ymin = self.rect.getP1().getY()
+        xmax = self.rect.getP2().getX()
+        ymax = self.rect.getP2().getY()
+        return (xmin <= x <= xmax)and(ymin <= y <= ymax)
+
+    def setFunc(self, func):
+        self.func = func
+            
--- main.py
+++ main.py
@@ -1,48 +1,92 @@
 from tablero import Tablero
-from graphics import GraphWin,color_rgb,Rectangle
+from button import Button
+from graphics import GraphWin, color_rgb, Rectangle, Point, Text
 
-tablero = Tablero(15,8)
-butStart = Rectangle()
-boolStart = False
-butWalls = Rectangle()
-boolWalls = False
-butRobot = Rectangle()
-boolRobot = False
-butClear = Rectangle()
-boolClear = False
+tablero = Tablero(15, 8)
+
+
+butStart = Button(5, 5, 100, 40, "Start/Stop", True)
+butWalls = Button(110, 5, 100, 40, "Walls", True)
+butRobot = Button(215, 5, 100, 40, "Roboto", True)
+butClear = Button(320, 5, 100, 40, "Clear", False)
+
+
+def func_start(pressed):
+    if(pressed):
+        print(":)")
+    else:
+        print(":(")
+
+
+def func_walls(pressed):
+    if(pressed):
+        print(":)")
+    else:
+        print(":(")
+
+
+def func_robot(pressed):
+    if(pressed):
+        print(":)")
+    else:
+        print(":(")
+
+
+def func_clear(pressed):
+    setText("Tablero limpiado!")
+
+butStart.setFunc()
+butWalls.set
+butRobot.draw(win)
+butClear.draw(win)
+
+info = Rectangle(Point(425,5),Point(745,45))
+infoText = Text(info.getCenter(),"")
 
 def main():
 
-    tablero = Tablero(15,8)
-    win = GraphWin("Practica 1 - Robotito :)", tablero.cols*tablero.cell_size, tablero.rows*tablero.cell_size + 50)
-    win.setBackground(color_rgb(192,192,192))
+    tablero = Tablero(15, 8)
+    global win 
+    win = GraphWin("Practica 1 - Robotito :)", tablero.cols *
+                   tablero.cell_size, tablero.rows*tablero.cell_size + 50)
+    win.setBackground(color_rgb(192, 192, 192))
     tablero.draw(win)
+    butStart.draw(win)
+    butWalls.draw(win)
+    butRobot.draw(win)
+    butClear.draw(win)
 
     while True:
         point = win.getMouse()
         xpos = point.getX()
         ypos = point.getY()
         if(ypos > 50):
-            tablero.toggle(xpos,ypos-50,win)
+            tablero.toggle(xpos, ypos-50, win)
+        else:
+            if(butStart.isInside(xpos, ypos)): butStart.press(win)
+            elif(butWalls.isInside(xpos, ypos)): butWalls.press(win)
+            elif(butRobot.isInside(xpos, ypos)): butRobot.press(win)
+            elif(butClear.isInside(xpos, ypos)): butClear.press(win)
+
+
 
     #win = GraphWin("My Circle", width, height)
     #square = Rectangle(Point(cell_size,cell_size), Point(width-cell_size, height-cell_size))
     #square.setFill(color_rgb(230, 230, 230))
-    #square.draw(win)
-    
-    #for i in range(1, columns-1):
+    # square.draw(win)
+
+    # for i in range(1, columns-1):
     #    line = Line(Point(i*cell_size,cell_size),Point(i*cell_size,height-cell_size))
     #    line.draw(win)
-    #for j in range(1, rows-1):
+    # for j in range(1, rows-1):
     #    line = Line(Point(cell_size,j*cell_size),Point(width-cell_size,j*cell_size))
-    #    line.draw(win)  
-        #for i in range(1, columns-1):
+    #    line.draw(win)
+        # for i in range(1, columns-1):
         #    for j in range(1, rows-1):
         #        square = Square(i, j, cell_size)
         #        square.draw(win)
- 
-    
-    #while True:
+
+    # while True:
     #    point = win.getMouse()
     #    posx = point.getX()
     #    posy = point.getY()
@@ -52,7 +96,14 @@ def main():
     #    card.setFill(color_rgb(192,192,192))
     #    card.draw(win)
 
-    #win.getMouse() # Pause to view result
-    #win.close()    # Close window when done
+    # win.getMouse() # Pause to view result
+    # win.close()    # Close window when done
+
+def setText(texto):
+    infoText.setText(texto)
+    infoText.undraw()
+    infoText.draw(win)
+
+
 
 main()
